705a3f91b145bddf68128a935f4dae8e7916ba82
edwin.chen <edwin.chen@bamboonetworks.com>
2019-05-10 16:49:35 +0800

    update

---
+++ src/components/CreateRecord.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-layout row wrap>
+    <v-flex xs8>
+      <v-text-field
+        color="green"
+        :placeholder="placeholder"
+        :value="value"
+        @input="$emit('onInput', $event)"
+        @keyup.enter="$emit('create')"
+      ></v-text-field>
+    </v-flex>
+    <v-flex xs4>
+      <v-btn color="green" dark class="mt-2" @click="$emit('create')">
+        <v-icon class="mr-1">add_circle</v-icon>Create
+      </v-btn>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  props: [
+    'placeholder',
+    'value',
+  ],
+};
+</script>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,63 +1,38 @@
 <template>
   <Panel title="Projects">
     <div class="mb-2 project" v-for="project in projects" :key="project.id">
-      <v-layout>
-        <v-flex xs9 class="text-xs-left">
-          <span v-if="!project.isEditMode">
-            {{ project.title }}
-          </span>
-          <v-text-field
-            autofocus
-            color="green"
-            v-if="project.isEditMode"
-            :value="project.title"
-            @keyup.enter="saveProject(project)"
-            @input="setProjectTitle({project, title: $event})"
-          >
-          </v-text-field>
-        </v-flex>
-        <v-flex xs3>
-          <v-icon
-            v-if="!project.isEditMode"
-            class="mt-1 mr-2"
-            @click="setEditMode(project)"
-          >edit</v-icon>
-          <v-icon
-            v-if="project.isEditMode"
-            class="mt-4 mr-2"
-            @click="saveProject(project)"
-          >check</v-icon>
-          <v-icon
-            @click="deleteProject(project)"
-          >delete</v-icon>
-        </v-flex>
-      </v-layout>
+      <EditableRecord
+        :isEditMode="project.isEditMode"
+        :title="project.title"
+        @onInput="setProjectTitle({
+          project,
+          title: $event,
+        })"
+        @onEdit="setEditMode(project)"
+        @onSave="saveProject(project)"
+        @onDelete="deleteProject(project)"
+      />
     </div>
-    <v-layout row wrap>
-      <v-flex xs8>
-        <v-text-field
-        color="green"
-        placeholder="My Project name"
-        @input="setNewProjectsName"
-        :value="newProjectsName"
-        @keyup.enter="createProject"
-      ></v-text-field>
-      </v-flex>
-      <v-flex xs4>
-        <v-btn color="green" dark class="mt-2" @click="createProject">
-          <v-icon class="mr-1">add_circle</v-icon>Create
-        </v-btn>
-      </v-flex>
-    </v-layout>
+    <CreateRecord
+      placeholder="My Project name"
+      @onInput="setNewProjectsName"
+      :value="newProjectsName"
+      @create="createProject"
+    />
   </Panel>
 </template>
 
 <script>
 import { mapMutations, mapState, mapActions } from 'vuex';
-
+import CreateRecord from '@/components/CreateRecord.vue';
+import EditableRecord from '@/components/EditableRecord.vue';
 export default {
   mounted() {
     this.fetchProject();
+  },
+  components: {
+    CreateRecord,
+    EditableRecord,
   },
   computed: {
     ...mapState('projects', [
